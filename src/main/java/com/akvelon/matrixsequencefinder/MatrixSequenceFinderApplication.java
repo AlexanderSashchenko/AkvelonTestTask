215e3f5395fb46fa9c716d32618b6efbc676496a
Alexandr Sashchenko <santys87@gmail.com>
2020-06-11 17:10:44 +0300

    first commit

---
+++ src/main/java/com/akvelon/matrixsequencefinder/MatrixSequenceFinderApplication.java
@@ -0,0 +1,13 @@
+package com.akvelon.matrixsequencefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixSequenceFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatrixSequenceFinderApplication.class, args);
+    }
+
+}
